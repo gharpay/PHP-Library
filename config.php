8da43fd7d0907cae3e043ad0a86a7f5ad3604af2
Khaja Naquiuddin <khaja@gharpay.in>
2012-03-24 21:23:46 +0530

    added missing product description feature

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-define("USERNAME","test_api"); // add your username here;
-define("PASSWORD","test_api"); // add password here;
+define("USERNAME",""); // add your username here;
+define("PASSWORD",""); // add password here;
 define("URL","http://services.gharpay.in"); //Your web service URL goes here;
 define("ERROR_ON",FALSE); //if you are developing app turn this to TRUE to enable error reporting.
 ?>
